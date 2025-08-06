93f54c502224e522584faa305abea0efafe8aad1
rambabai52 <rambabaabai129@gmail.com>
2025-08-06 17:56:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask app deployed successfully!"
+   return "Hello, this is my updated Flask app!" 
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
